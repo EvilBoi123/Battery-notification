83e37ae5a33ac7f76543ab7c108656e208b87d74
EvilBoi123 <muhammadalif3003@gmail.com>
2025-05-23 17:40:27 +0800

    transitioning from config specifc to function specifc, as in definition from file names

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 use std::{fs, thread, time::Duration, process::Command, path::{Path, PathBuf}}; 
 use once_cell::sync::Lazy; 
 
+static ICON: Lazy<PathBuf> = Lazy::new(|| CURRENT_DIR.join("lowBattery.jpg"));
+static SOUND: Lazy<PathBuf> = Lazy::new(|| CURRENT_DIR.join("Google-notification.mp3"));
+static STATUS_PATH: Lazy<PathBuf> = Lazy::new(|| CURRENT_DIR.join("status"));
+static CAPACITY_PATH: Lazy<PathBuf> = Lazy::new(|| CURRENT_DIR.join("capacity"));
+
+
 static CURRENT_DIR: Lazy<PathBuf> = Lazy::new(||{ 
     std::env::current_exe()
         .expect("Failed to get exe")
@@ -10,28 +16,27 @@ static CURRENT_DIR: Lazy<PathBuf> = Lazy::new(||{
     });
 
 fn main(){
-    battery_check(); 
+    let battery_monitor = thread::spawn(move || {
+    battery_notify(); 
+    });
+    battery_monitor.join();
+
 }
 
-fn battery_check() {
 
-    let icon = CURRENT_DIR.join("lowBattery.jpg");
-    let sound = CURRENT_DIR.join("Google-notification.mp3");
+fn battery_notify() {
 
-    // Temporary
-    let status_path = CURRENT_DIR.join("status");
-    let capacity_path = CURRENT_DIR.join("capacity"); 
-
+    
     let mut notified_low = false; 
     let mut notified_critical = false; 
     let mut notified_full = false; 
 
     loop {
-        let status = fs::read_to_string(&status_path)
+        let status = fs::read_to_string(&*STATUS_PATH)
             .unwrap_or_default()
             .trim()
             .to_string();
-        let capacity: u32 = fs::read_to_string(&capacity_path)
+        let capacity: u32 = fs::read_to_string(&*CAPACITY_PATH)
             .unwrap_or_default()
             .trim()
             .parse()
@@ -40,7 +45,7 @@ fn battery_check() {
         match (status.as_str(), capacity) {
             ("Discharging", 10..=20) => {
                 if !notified_low {
-                    notify("~Oni-Chan!", &format!("Your battery is low at {}", capacity), &icon, &sound);
+                    notify("~Oni-Chan!", &format!("Your battery is low at {}", capacity), &*ICON, &*SOUND);
                     notified_low = true;
                     notified_critical = false;
                     notified_full = false; 
@@ -48,7 +53,7 @@ fn battery_check() {
             }
             ("Discharging", 0..=9) => {
                 if !notified_critical {
-                    notify("~Oni-chan! I'm really hungry!", "Please feed me!",&icon, &sound);
+                    notify("~Oni-chan! I'm really hungry!", "Please feed me!",&*ICON, &*SOUND);
                     notified_low = false; 
                     notified_critical = true; 
                     notified_full = false; 
@@ -56,7 +61,7 @@ fn battery_check() {
             }
             ("Discharging", 95..) => {
                 if !notified_full {
-                    notify("~Oni-chan!", "I'm filled!", &icon, &sound);
+                    notify("~Oni-chan!", "I'm filled!", &*ICON, &*SOUND);
                     notified_low = false; 
                     notified_critical = false; 
                     notified_full = true; 
